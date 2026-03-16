4ccb1e26fdead57319e11e37049b8b1e806f925d
Omri H <omrih.e@protonmail.com>
2026-03-16 17:22:53 +0200

    plymouth boot screen

--- global.nix
+++ global.nix
@@ -10,8 +10,22 @@
   };
   nix.package = pkgs.lixPackageSets.stable.lix;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    plymouth = {
+      enable = true;
+    };
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
   services.locate.enable = true;
 
