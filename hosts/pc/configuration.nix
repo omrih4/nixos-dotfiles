f7c2dc27f5940f076c1e0e967dc89c982e736060
Omri H <omrih.e@protonmail.com>
2026-05-07 16:55:16 +0300

    enable nvenc for sunshine

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -24,6 +24,9 @@
 
   services.sunshine = {
     enable = true;
+    package = pkgs.sunshine.override {
+      cudaSupport = true;
+    };
     autoStart = true;
     capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
     openFirewall = true;
